4ab0a17888e03440656e7cb5f7f6e71d111475f7
Artur Pietracha <artur.pietracha@gmail.com>
2024-07-16 07:38:26 +0200

    Fix CollectionsByProductIdLoader if collection is missing

--- saleor/graphql/product/dataloaders/products.py
+++ saleor/graphql/product/dataloaders/products.py
@@ -537,7 +537,6 @@ class CollectionsByProductIdLoader(DataLoader):
     def batch_load(self, keys):
         product_collection_pairs = list(
             CollectionProduct.objects.using(self.database_connection_name)
-            .using(self.database_connection_name)
             .filter(product_id__in=keys)
             .order_by("id")
             .values_list("product_id", "collection_id")
@@ -548,9 +547,13 @@ class CollectionsByProductIdLoader(DataLoader):
             product_collection_map[pid].append(cid)
 
         def map_collections(collections):
-            collection_map = {c.id: c for c in collections}
+            collection_map = {c.id: c for c in collections if c is not None}
             return [
-                [collection_map[cid] for cid in product_collection_map[pid]]
+                [
+                    collection_map[cid]
+                    for cid in product_collection_map[pid]
+                    if cid in collection_map
+                ]
                 for pid in keys
             ]
 
